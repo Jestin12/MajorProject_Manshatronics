05942790a1885db91ba1bc1bff6b9147f587f0b7
Jestin12 <jestinji@outlook.com>
2023-05-25 04:27:25 +1000

    I've updated RemotePuzzleWithI2C its fully integrated

--- SimonSaysWithTimer/Core/Src/main.c
+++ SimonSaysWithTimer/Core/Src/main.c
@@ -674,6 +674,7 @@ void initialise_board() {
 	// get a pointer to the second half word of the MODER register (for outputs pe8-15)
 	uint16_t *led_output_registers = ((uint16_t *)&(GPIOE->MODER)) + 1;
 	*led_output_registers = 0x5555;
+
 }
 
 
