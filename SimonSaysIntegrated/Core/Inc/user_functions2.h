6d8e770b603a03b85ddcf00a6aac9ad45bb143c1
Jestin12 <jestinji@outlook.com>
2023-05-25 04:33:11 +1000

    SimonSays updated this version is fully integrated

---
+++ SimonSaysIntegrated/Core/Inc/user_functions2.h
@@ -0,0 +1,19 @@
+/*
+ * user_functions2.h
+ *
+ *  Created on: May 18, 2023
+ *      Author: jesti
+ */
+
+#ifndef INC_USER_FUNCTIONS2_H_
+#define INC_USER_FUNCTIONS2_H_
+
+int level1();
+
+int level2();
+
+int level3();
+
+int level4();
+
+#endif /* INC_USER_FUNCTIONS2_H_ */
